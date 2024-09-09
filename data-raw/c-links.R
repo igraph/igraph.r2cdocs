f19210c14e96d54b774885683469a9302f0a9dbc
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-09 12:09:22 +0200

    fix: update links thx @szhorvat

--- data-raw/c-links.R
+++ data-raw/c-links.R
@@ -7,7 +7,7 @@ entries <- xml2::xml_find_all(index, ".//dt")
 
 handle_dt <- function(dt) {
   href <- xml2::xml_attr(xml2::xml_child(dt), "href")
-  url <- sprintf("https://igraph.org/c/doc/%s", href)
+  url <- sprintf("https://igraph.org/c/html/latest/%s", href)
   method <- sub(".*#", "", href)
   tibble::tibble(
     method = method,
