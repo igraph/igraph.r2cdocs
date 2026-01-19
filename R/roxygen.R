ec4ef71d3ab8045611c56150388a3fd6fb081d79
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-01-19 13:46:28 +0100

    chore: roclet not needed

--- R/roxygen.R
+++ R/roxygen.R
@@ -16,16 +16,15 @@ roxy_tag_rd.roxy_tag_cdocs <- function(x, base_path, env) {
 format.rd_section_cdocs <- function(x, ...) {
   paste0(
     "\\section{Related documentation in the C library}{",
-  present_cdocs_link(x[["value"]]),
+    present_cdocs_link(x[["value"]]),
     ".}\n"
   )
 }
 
 present_cdocs_link <- function(value) {
-
   format_cdocs_single_link <- function(x, clinks) {
-  # we can use igraph_ or not
-    df <- clinks[clinks$method %in% c(x, sprintf("igraph_%s", x)),]
+    # we can use igraph_ or not
+    df <- clinks[clinks$method %in% c(x, sprintf("igraph_%s", x)), ]
     if (nrow(df) == 0) {
       cli::cli_warn("Can't find C entry for {x}!")
     }
@@ -35,26 +34,6 @@ present_cdocs_link <- function(value) {
   strings <- map_chr(unique(value), format_cdocs_single_link, clinks = clinks())
 
   paste(strings, collapse = ", ")
-}
-
-
-#' @export
-cdocs_roclet <- function() {
-  roxygen2::roclet("cdocs")
-}
-
-#' @importFrom roxygen2 block_get_tags roclet_process
-#' @method roclet_process roclet_cdocs
-#' @export
-roclet_process.roclet_cdocs <- function(x, blocks, env, base_path) {
-  x
-}
-
-
-#' @export
-#' @importFrom roxygen2 block_get_tags roclet_output
-roclet_output.roclet_cdocs <- function(x, results, base_path, ...) {
-  x
 }
 
 clinks <- function() {
